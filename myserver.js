e3da46a24903fe7c694b1122598bfbbd950c533d
karmjeet <karmdinho@gmail.com>
2017-02-16 11:53:33 +0530

    setup heroku port and  start script

--- myserver.js
+++ myserver.js
@@ -1,19 +1,20 @@
 const express =require('express');
 const hbs=require('hbs');  //it is on of the template engine used in express
-
+const port = process.env.PORT||3000;
 var app = express();//passing express as the function
 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs');
 
-app.use(express.static(__dirname+'/playarena')) //use is used to take middleware function  
 hbs.registerHelper('currentYear',()=>{  //i have register a helper which lets me create a function and lets me use it in a template
                    return new Date().getFullYear();
                    });
-app.use((req,res,next)=>{
-    res.render('maintainance.hbs');
+//app.use((req,res,next)=>{
+//    res.render('maintainance.hbs');
+//
+//});
 
-});
+app.use(express.static(__dirname+'/playarena')) //use is used to take middleware function  
 
 app.get('/', (req,res) => {
     res.render('home.hbs',{             //we are rendering the page so that we can dynamicalyy add or update ti html template
@@ -35,6 +36,6 @@ app.get('/about',(req,res)=>{
 });
 
 
-app.listen(3000,()=>{
-    console.log('server is up on port 3000');
+app.listen(port,()=>{
+    console.log(`server is runnig on ${port} Port`);
 });
